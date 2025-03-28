6ee598673a6cb1829620a8544971082a86fb3615
x1shang <971374215@qq.com>
2025-03-28 11:29:10 +0800

    一个可以成功运行的GUI测试文件

--- GUI测试.py
+++ GUI测试.py
@@ -1,20 +1,14 @@
-from PySide2.QtWidgets import QApplication, QMainWindow, QPushButton,  QPlainTextEdit
+
+from PySide2.QtWidgets import QApplication, QMessageBox, QWidget
+from PySide2.QtUiTools import QUiLoader
+
+class Stats:
+
+    def __init__(self):
+        self.ui = QUiLoader().load('calculator.ui')
+
 
 app = QApplication([])
-
-window = QMainWindow()
-window.resize(500, 400)
-window.move(300, 310)
-window.setWindowTitle('薪资统计')
-
-textEdit = QPlainTextEdit(window)
-textEdit.setPlaceholderText("请输入薪资表")
-textEdit.move(10,25)
-textEdit.resize(300,350)
-
-button = QPushButton('统计', window)
-button.move(380,80)
-
-window.show()
-
-app.exec_() # PySide2 是 exec_
+stats = Stats()
+stats.ui.show()
+app.exec_()
